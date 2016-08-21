d48202860bf115e63df22ae5178556432ac5dfa4
Attila Kiss <ato@Ato-Mac-mini-3.local>
2016-08-21 12:27:29 +0200

    angol megjegyzések kivétele

--- www/js/ajax.js
+++ www/js/ajax.js
@@ -1,12 +1,9 @@
 function ajax_hivas(url,callback,ID)
 {
 				url = url + "&callback="+callback;
-				// create a new script element
 				var script = document.createElement('script');
-				// set the src attribute to that url
 				script.setAttribute("id",ID);
 				script.setAttribute('src', url);
-				// insert the script in out page
 				document.getElementsByTagName('head')[0].appendChild(script);
 }
 
